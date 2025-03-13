abf97cfb95e93f41515fe57bffe77c1fad074755
Jorge Mateus <jorgemateus1995@gmail.com>
2025-03-12 19:44:40 -0500

    Se agregan los nuevos assets para la sección de Diseño Gráfico y se implementan las tarjetas para cada proyecto nuevo; se modifica el script de script-diseno.js para popular la galeria de cada proyecto de manera dinamica y se agrega un eventListener para cerrar la galeria con la tecla "Esc"; se hacen correcciones menores de estilo en style-diseno.css y style-general.css

--- src/javascript/script-diseno.js
+++ src/javascript/script-diseno.js
@@ -1,11 +1,31 @@
-import * as calendarios from "../img/Diseno/Calendarios/*.jpg";
 import * as carteles from "../img/Diseno/Carteles/*.webp";
 import * as logos from "../img/Diseno/*.jpg";
+import * as corral from "../img/Diseno/Camapana_el_corral/*.webp"
+import * as frisby from "../img/Diseno/Campana_frisby/*.webp"
+import * as cordilleras from "../img/Diseno/Campana_tres_cordilleras/*.webp"
+import * as universidades from "../img/Diseno/Campana_universidades/*.webp"
+import * as xp from "../img/Diseno/Campana_XP/*.webp"
 
 const main = document.querySelector("main");
 const cards = document.querySelectorAll(".project-card");
 const imgContainer = document.querySelector(".img-container");
 const overlay = document.querySelector(".overlay-diseno");
+
+const hideGallery = function (e) {
+  e.stopPropagation();
+  if (window.screen.width <= 832) {
+    overlay.style.top = null;
+    main.style.touchAction = null;
+  }
+
+  if(e.key === "Escape" && e.type === "keydown"){
+    overlay.classList.add("hidden-diseno");
+    imgContainer.innerHTML = "";
+  } else if (e.type === "click") {
+    overlay.classList.add("hidden-diseno");
+    imgContainer.innerHTML = "";
+  }
+}
 
 const showGallery = function (e) {
   overlay.classList.remove("hidden-diseno");
@@ -17,13 +37,6 @@ const showGallery = function (e) {
   const content = e.target.parentNode.dataset.content;
 
   switch (content) {
-    case "calendarios":
-      for (let i = 1; i <= Object.keys(calendarios).length; i++) {
-        const imageElement = document.createElement("img");
-        imageElement.src = calendarios[i];
-        imgContainer.appendChild(imageElement);
-      }
-      break;
     case "carteles":
       const keysCarteles = Object.keys(carteles);
       keysCarteles.forEach((k) => {
@@ -41,6 +54,51 @@ const showGallery = function (e) {
         imgContainer.appendChild(imageElement);
       });
       break;
+    case "corral":
+      const keysCorral = Object.keys(corral);
+      keysCorral.forEach((k) => {
+        const imageElement = document.createElement("img");
+        imageElement.src = corral[k];
+        imageElement.classList.add("img-tall");
+        imgContainer.appendChild(imageElement);
+      });
+      break;
+    case "frisby":
+      const keysFrisby = Object.keys(frisby);
+      keysFrisby.forEach((k) => {
+        const imageElement = document.createElement("img");
+        imageElement.src = frisby[k];
+        imageElement.classList.add("img-tall");
+        imgContainer.appendChild(imageElement);
+      });
+      break;
+    case "cordilleras":
+      const keysCordilleras = Object.keys(cordilleras);
+      keysCordilleras.forEach((k) => {
+        const imageElement = document.createElement("img");
+        imageElement.src = cordilleras[k];
+        imageElement.classList.add("img-tall");
+        imgContainer.appendChild(imageElement);
+      });
+      break;
+    case "universidades":
+      const keysUniversidades = Object.keys(universidades);
+      keysUniversidades.forEach((k) => {
+        const imageElement = document.createElement("img");
+        imageElement.src = universidades[k];
+        imageElement.classList.add("img-tall");
+        imgContainer.appendChild(imageElement);
+      });
+      break;
+    case "xp":
+      const keysXP = Object.keys(xp);
+      keysXP.forEach((k) => {
+        const imageElement = document.createElement("img");
+        imageElement.src = xp[k];
+        imageElement.classList.add("img-tall");
+        imgContainer.appendChild(imageElement);
+      });
+      break;
   }
 };
 
@@ -48,15 +106,9 @@ cards.forEach((c) => {
   c.addEventListener("click", showGallery);
 });
 
-overlay.addEventListener("click", (e) => {
-  e.stopPropagation();
-  if (window.screen.width <= 832) {
-    overlay.style.top = null;
-    main.style.touchAction = null;
-  }
-  overlay.classList.add("hidden-diseno");
-  imgContainer.innerHTML = "";
-});
+overlay.addEventListener("click", hideGallery);
+
+window.addEventListener("keydown", hideGallery)
 
 window.addEventListener("popstate", function (event) {
   event.preventDefault();
